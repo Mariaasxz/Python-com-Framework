c71d72c4c60b1fb7caa2f808d4429ec2f0d9f276
Duda <mvieiradasilva14@gmail.com>
2026-05-09 16:25:21 -0300

    Add files via upload

---
+++ 0905.py
@@ -0,0 +1,25 @@
+# for i in range(5):
+#     print("Oi")
+
+
+# for i in range(5, 0, -1):
+#     print(i)
+
+# print("BOOM!")
+
+
+# # 1
+# arquivo = open('cadastro.txt', 'r')
+# for linha in arquivo:
+#     dados = linha.strip().split(',')
+#     nome =  dados[0]
+#     idade = dados[1]
+#     print('Nome', nome, 'idade', idade)
+
+
+# arquivo.close()
+# # 2
+# with open('cadastro.csv', 'r') as c:
+#     conteudo = c.read()
+#     print(conteudo)
+
